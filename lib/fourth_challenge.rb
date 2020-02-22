c5a87f85ab798936fb38fad4b4be73c8a6551ae5
Tisdale Fry <tisdalefry@gmail.com>
2020-02-22 04:41:02 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,8 +20,8 @@ epic_tragedy = {
    }
 }
 
-epic_tragedy[:montague][:hero][:status] = "dead"
+puts epic_tragedyepic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
-puts epic_tragedy
+
 
 end
